3ca2fa7bccf6ce3c1650c5dc5ecc424772f8b16c
Dmytrii27 <dima.satsiuk2707@gmail.com>
2024-07-03 06:07:07 +0200

    df

---
+++ practice2.js
@@ -0,0 +1,19 @@
+
+const num = 4;
+
+
+    
+if (num > 0) {
+    console.log("The number is positive");
+}
+
+
+else if (num == 0) {
+  console.log("The number is zero");
+}
+
+
+else {
+     console.log("The number is negative");
+}
+
