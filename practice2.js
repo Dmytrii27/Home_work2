cc6f92f6148660d1a2a226564bb46b61fea7c3d7
Dmytrii27 <dima.satsiuk2707@gmail.com>
2024-07-03 06:07:22 +0200

    df

--- practice2.js
+++ practice2.js
@@ -1,19 +1,54 @@
 
-const num = 4;
+ let num = 0;
 
-
-    
-if (num > 0) {
-    console.log("The number is positive");
-}
-
-
-else if (num == 0) {
-  console.log("The number is zero");
-}
-
-
+  if (num > 0) {
+    console.log("Positive");
+  }
+  else if (num == 0) {
+    console.log("Zero");}
 else {
-     console.log("The number is negative");
+  console.log("Negative");
 }
+
+
+
+
+function printEvenNumbers() {
+  for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+  }
+}
+printEvenNumbers();
+
+function countdown(i) {
+  
+  answer = '';
+  while( i >= 0) {
+     answer = answer + i.toString() + ' ';
+     i--;
+     console.log(i);
+  }
+  return answer;
+ 
+  
+}
+
+
+countdown(4);
+
+let guess = parseInt(prompt("Guess a number between 1 and 10: "), 10);
+if (guess > 10) {
+  alert("Reed rules again");
+  console.log(parseInt(prompt("Guess a number between 1 and 10: "), 10));
+}
+else if (guess == 10)
+  alert("You won");
+else {
+  alert("Try again!");
+  console.log(parseInt(prompt("Guess a number between 1 and 10: "), 10));
+  alert("Try again!");
+}
+function myFunction() {
+  document. getElementById("demo"). innerText = 'You have clicked';
+  }
 
---
+++ practice_script.js
@@ -0,0 +1,3 @@
+function myFunction() {
+document. getElementById("demo"). innerText = 'You have clicked';
+}
