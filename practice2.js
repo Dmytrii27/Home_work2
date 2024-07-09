e4f3c9ebfcac9fabc5d50ae3945300427538bf2d
Dmytrii27 <dima.satsiuk2707@gmail.com>
2024-07-10 01:22:44 +0200

    аа

--- practice2.js
+++ practice2.js
@@ -36,7 +36,7 @@ function countdown(i) {
 
 countdown(4);
 
-let guess = parseInt(prompt("Guess a number between 1 and 10: "), 10);
+/*let guess = parseInt(prompt("Guess a number between 1 and 10: "), 10);
 if (guess > 10) {
   alert("Reed rules again");
   console.log(parseInt(prompt("Guess a number between 1 and 10: "), 10));
@@ -51,4 +51,36 @@ else {
 function myFunction() {
   document. getElementById("demo"). innerText = 'You have clicked';
   }
+ 
+    const Ctemp = [0, 10, 20, 30, 40, 50];
+     const Ctemp2 = Ctemp.map( (x) => x * 9 / 5 + 32);
+    
+    
+    console.log(Ctemp2);
 
+    const hideEvenNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const evenNumbers = hideEvenNumbers.filter(function(element) {
+      return element % 2 === 0;
+});
+console.log(evenNumbers);*/
+
+const students = [
+  { name: 'Alice', score: 85 },
+  { name: 'Bob', score: 58 },
+  { name: 'Charlie', score: 92 },
+  { name: 'David', score: 45 },
+  { name: 'Eve', score: 70 },
+  { name: 'Frank', score: 55 },
+  { name: 'Grace', score: 60 }
+];
+console.log(students.filter(person => person.score >= 60 )
+.map((student) => `${student.name}: ${student.score}`))
+
+
+;
+
+
+
+   
+
+    
