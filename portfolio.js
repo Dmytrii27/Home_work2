30e5dd4afdf998bcde749b54b3e0718d31efc0cb
Dmytrii27 <dima.satsiuk2707@gmail.com>
2024-07-20 01:14:18 +0200

    рр

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const spans = document.querySelectorAll(".container span");
+
+
+//document.addEventListener("DOMContentLoaded", function() {
+    //const spans = document.querySelectorAll(".container span");
   
-    spans.forEach(span => {
-      span.addEventListener("animationend", () => {
-        span.style.animation = "none"; // Зупиняємо анімацію
-      });
-    });
-  });
+    //spans.forEach(span => {
+      //span.addEventListener("animationend", () => {
+       // span.style.animation = "none"; // Зупиняємо анімацію
+     // });
+   // });
+ // });
+
+  
+  
